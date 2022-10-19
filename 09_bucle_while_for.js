33d131af7639b59d4e028393a790442d14669ada
JotaConts <jotape.desenfocado@gmail.com>
2022-10-19 06:19:19 -0300

    switch case

--- 09_bucle_while_for.js
+++ 09_bucle_while_for.js
@@ -12,7 +12,43 @@ do {
   n++;
 } while (n < 3);
 
-// Bucle for:
-for (let i = 0; i < 3; i++) { // muestra 0, luego 1, luego 2
-    alert(i);
+// Bucle for (range):
+for (let i = 0; i < 3; i++) {
+    console.log(i);
   }
+
+// También podemos eliminar la parte step:
+let e = 0;
+for (; e < 3;) {
+  console.log( e++ );
+}
+// Esto hace al bucle idéntico a un bucle while (e < 3)
+
+
+// Podemos forzar la salide con 'break' y saltar una iteración con 'continue'
+
+// ETIQUETAS
+// Si estamos en un bucle anidado y queremos salir de él pero a la vez del principal,
+// podemos usar una etiqueta que nos saque de ambos bucles de inmediato.
+// Nombramos la etiqueta seguido de dos puntos antes de la declaració del for.
+// Dentro del for anidado idicamos la condición seguido de 'break <etiqueta>'.
+
+outer: for (let i = 0; i < 3; i++) {
+
+    for (let j = 0; j < 3; j++) {
+  
+      let input = prompt(`Value at coords (${i},${j})`, '');
+  
+      // Si es una cadena de texto vacía o se canceló, entonces salir de ambos bucles
+      if (!input) break outer; // (*)
+  
+      // hacer algo con el valor...
+    }
+  }
+
+//También podemos mover la etiqueta a una línea separada:
+
+// outer:
+// for (let i = 0; i < 3; i++) { ... }
+//   console.log('Listo!');
+
