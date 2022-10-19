bc1631635e1813e8ee541d6691878e627af239d6
JotaConts <jotape.desenfocado@gmail.com>
2022-10-19 05:31:32 -0300

    bucles while y for

---
+++ 09_bucle_while_for.js
@@ -0,0 +1,18 @@
+// Bucle while:
+let i = 0;
+while (i < 3) { // muestra 0, luego 1, luego 2
+  console.log(i);
+  i++;
+}
+
+// Bucle do while:
+let n = 0;
+do {
+  console.log(n);
+  n++;
+} while (n < 3);
+
+// Bucle for:
+for (let i = 0; i < 3; i++) { // muestra 0, luego 1, luego 2
+    alert(i);
+  }
